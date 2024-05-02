b63fde529084744ac7a6228c667a6d4581212c06
Riyan Gonsalves <riyan0350@gmail.com>
2024-05-02 21:08:20 +0530

    add mode code from unity

--- scripts/Wing.cs
+++ scripts/Wing.cs
@@ -13,6 +13,8 @@ public partial class Wing : Node3D
     [Export]
     private float Chord = 3f;
     [Export]
+    private float Span = 10f;
+    [Export]
     private float ChordRatio = .2f;
     [Export]
     private float DeltaFlapAngle = .3f;
@@ -21,13 +23,17 @@ public partial class Wing : Node3D
     [Export]
     private float IndicatorScale = .3f;
 
-    private float CLAlpha;
-    private float SurfaceArea;
+    private float _cLAlpha;
+    private float _surfaceArea;
+    private float _liftCoeffNegMaxBase;
+    private float _liftCoeffPosMaxBase;
+
     private Player _aircraft;
     private Global _global;
     private MeshInstance3D _indicator;
     private Vector3 _velocity;
     private Vector3 _wind;
+    private float _dragCoeffAtStall;
     private float _coeffOfLift;
     private float _coeffOfDrag;
     private float _coeffOfTorque;
@@ -39,8 +45,8 @@ public partial class Wing : Node3D
 
     public Wing()
     {
-        SurfaceArea = Chord * Chord * AspectRatio;
-        _cNu = 1 - Mathf.Exp(-17/AspectRatio);
+        _surfaceArea = Chord * Span;
+        _cNu = 1 - Mathf.Exp(-17 / AspectRatio);
     }
 
     public override void _Ready()
@@ -48,7 +54,9 @@ public partial class Wing : Node3D
         _aircraft = GetOwner<Player>();
         _global = GetNode<Global>("/root/Global");
         _indicator = GetNode<MeshInstance3D>("Indicator");
-        CLAlpha = _global.LiftCurveSlope * (AspectRatio / (AspectRatio + 2 * (AspectRatio + 4)/(AspectRatio + 2)));
+        _cLAlpha = _global.LiftCurveSlope * (AspectRatio / (AspectRatio + 2 * (AspectRatio + 4) / (AspectRatio + 2)));
+        _liftCoeffPosMaxBase = _cLAlpha * (_global.AlphaStallPosBase - ZeroAOABase) + _global.DeltaCLMax;
+        _liftCoeffNegMaxBase = _cLAlpha * (_global.AlphaStallNegBase - ZeroAOABase) + _global.DeltaCLMax;
     }
 
     public override void _Process(double delta)
@@ -66,37 +74,56 @@ public partial class Wing : Node3D
 
     private void UpdateCoefficients()
     {
-        float alpha, cT, cN, ZeroAOA;
+        float alpha, zeroLiftAoA;
         float theta = Mathf.Acos(2 * ChordRatio - 1);
-        float tau = 1 - (theta - Mathf.Sin(theta))/Mathf.Pi;
+        float tau = 1 - (theta - Mathf.Sin(theta)) / Mathf.Pi;
 
-        _coeffOfLift -= CLAlpha * tau * _global.Viscosity * _flapAngle;
+        zeroLiftAoA = ZeroAOABase - tau * _global.Viscosity * _flapAngle;
 
-        ZeroAOA = ZeroAOABase - tau * _global.Viscosity * _flapAngle;
+        _dragCoeffAtStall = -4.26e-2f * _flapAngle * _flapAngle + 2.1e-1f * _flapAngle + 1.98f;
 
-        alpha = (_velocity.Y == 0 && _velocity.Z == 0) ? 0 : Mathf.Atan2(-_velocity.Y, _velocity.Z);
-        _coeffOfLift = CLAlpha * (alpha - ZeroAOA);
-        alpha = alpha - ZeroAOA - _coeffOfLift/(Mathf.Pi * AspectRatio);
+        alpha = Mathf.Atan2(_velocity.Y, _velocity.Z);
 
-        float alphaStallPos = ZeroAOA + _global.LiftCoeffPosMaxBase/CLAlpha;
-        float alphaStallNeg = ZeroAOA + _global.LiftCoeffNegMaxBase/CLAlpha;
+        float alphaStallPos = zeroLiftAoA + _liftCoeffPosMaxBase / _cLAlpha;
+        float alphaStallNeg = zeroLiftAoA + _liftCoeffNegMaxBase / _cLAlpha;
+
+        float paddingAnglePos = Mathf.DegToRad(Mathf.Lerp(15, 5, (Mathf.RadToDeg(_flapAngle) + 50) / 100));
+        float paddingAngleNeg = Mathf.DegToRad(Mathf.Lerp(15, 5, (-Mathf.RadToDeg(_flapAngle) + 50) / 100));
+        float paddedStallAnglePos = alphaStallPos - paddingAnglePos;
+        float paddedStallAngleNeg = alphaStallNeg - paddingAngleNeg;
 
         GD.PrintS(alpha, alphaStallPos, alphaStallNeg);
 
         if (alpha < alphaStallPos && alpha > alphaStallNeg)
         {
-            cT = SkinFrictionCoeff * Mathf.Cos(alpha);
-            cN = (_coeffOfLift + cT * Mathf.Sin(alpha))/Mathf.Cos(alpha);
-            _coeffOfDrag = cN * Mathf.Sin(alpha) + cT * Mathf.Cos(alpha);
-            _coeffOfTorque = -cN * (.25f - .175f * (1 - 2 * alpha / Mathf.Pi));
+            (_coeffOfLift, _coeffOfDrag, _coeffOfTorque) = CalculateCoeffsAtLowAoA(alpha, zeroLiftAoA);
         }
         else
         {
-            cN = SkinFrictionCoeff * Mathf.Sin(alpha) * (1/(.56f + .44f * Mathf.Sin(alpha)) - .41f * _cNu);
-            cT = .5f * SkinFrictionCoeff * Mathf.Cos(alpha);
-            _coeffOfLift = cN * Mathf.Cos(alpha) - cT * Mathf.Sin(alpha);
-            _coeffOfDrag = cN * Mathf.Sin(alpha) + cT * Mathf.Cos(alpha);
-            _coeffOfTorque = -cN * (.25f - .175f * (1f - 2f * alpha / Mathf.Pi));
+            if (alpha > paddedStallAnglePos || alpha < paddedStallAngleNeg)
+            {
+                (_coeffOfLift, _coeffOfDrag, _coeffOfTorque) = CalculateCoeffsAtStall(alpha, zeroLiftAoA, alphaStallPos, alphaStallNeg);
+            }
+            else
+            {
+                float lerpParam;
+                Vector3 aerodynamicCoefficientsLow;
+                Vector3 aerodynamicCoefficientsStall;
+
+                if (alpha > alphaStallPos)
+                {
+                    aerodynamicCoefficientsLow = CalculateCoeffsAtLowAoA(alphaStallPos, zeroLiftAoA);
+                    aerodynamicCoefficientsStall = CalculateCoeffsAtStall(paddedStallAnglePos, zeroLiftAoA, alphaStallPos, alphaStallNeg);
+                    lerpParam = (alpha - alphaStallPos) / (paddedStallAnglePos - alphaStallPos);
+                }
+                else
+                {
+                    aerodynamicCoefficientsLow = CalculateCoeffsAtLowAoA(alphaStallNeg, zeroLiftAoA);
+                    aerodynamicCoefficientsStall = CalculateCoeffsAtStall(paddedStallAngleNeg, zeroLiftAoA, alphaStallPos, alphaStallNeg);
+                    lerpParam = (alpha - alphaStallPos) / (paddedStallAnglePos - alphaStallPos);
+                }
+                (_coeffOfLift, _coeffOfDrag, _coeffOfTorque) = aerodynamicCoefficientsLow.Lerp(aerodynamicCoefficientsStall, lerpParam);
+            }
         }
 
     }
@@ -118,19 +145,19 @@ public partial class Wing : Node3D
 
     public Vector3 CalculateLift()
     {
-        _lift = _coeffOfLift * _global.AirDensity * _aircraft.LinearVelocity.Length() / 2 * SurfaceArea;
+        _lift = _coeffOfLift * _global.AirDensity * _aircraft.LinearVelocity.Length() / 2 * _surfaceArea;
         return _lift * Vector3.Up;
     }
 
     public Vector3 CalculateDrag()
     {
-        _drag = _coeffOfDrag * _global.AirDensity * _aircraft.LinearVelocity.Length() / 2 * SurfaceArea;
+        _drag = _coeffOfDrag * _global.AirDensity * _aircraft.LinearVelocity.Length() / 2 * _surfaceArea;
         return _drag * Vector3.Back;
     }
 
     public Vector3 CalculateRotatoryForce()
     {
-        _rotatoryForce = _coeffOfTorque * _global.AirDensity * _aircraft.LinearVelocity.Length() / 2 * SurfaceArea;
+        _rotatoryForce = _coeffOfTorque * _global.AirDensity * _aircraft.LinearVelocity.Length() / 2 * _surfaceArea;
         return _rotatoryForce * Vector3.Up;
     }
 
@@ -139,5 +166,49 @@ public partial class Wing : Node3D
         RibbonTrailMesh mesh = (RibbonTrailMesh)_indicator.Mesh;
         _indicator.Position = new Vector3(0, IndicatorScale * _lift, 0);
         mesh.SectionLength = _indicator.Position.Y;
+    }
+
+    private Vector3 CalculateCoeffsAtLowAoA(float alpha, float zeroLiftAOA)
+    {
+        float coeffOfLift = _cLAlpha * (alpha - zeroLiftAOA);
+        float alphaEffictive = alpha - zeroLiftAOA - coeffOfLift / (Mathf.Pi * AspectRatio);
+        float tangentialCoeff = SkinFrictionCoeff * Mathf.Cos(alphaEffictive);
+        float normalCoeff = (coeffOfLift + tangentialCoeff * Mathf.Sin(alphaEffictive)) / Mathf.Cos(alphaEffictive);
+        float coeffOfDrag = normalCoeff * Mathf.Sin(alphaEffictive) + tangentialCoeff * Mathf.Cos(alphaEffictive);
+        float coeffOfTorque = -normalCoeff * (.25f - .175f * (1 - 2 * Mathf.Abs(alphaEffictive) / Mathf.Pi));
+
+        return new Vector3(coeffOfLift, coeffOfDrag, coeffOfTorque);
+    }
+
+    private Vector3 CalculateCoeffsAtStall(float alpha, float zeroLiftAoA, float alphaStallPos, float alphaStallNeg)
+    {
+        float coeffOfLift;
+        if (alpha > alphaStallPos)
+        {
+            coeffOfLift = _cLAlpha * (alphaStallPos - zeroLiftAoA);
+        }
+        else
+        {
+            coeffOfLift = _cLAlpha * (alphaStallNeg - zeroLiftAoA);
+        }
+
+        float lerpParam;
+        if (alpha > alphaStallPos)
+        {
+            lerpParam = (Mathf.Pi / 2 - Mathf.Clamp(alpha, -Mathf.Pi / 2, Mathf.Pi / 2)) / (Mathf.Pi / 2 - alphaStallPos);
+        }
+        else
+        {
+            lerpParam = (-Mathf.Pi / 2 - Mathf.Clamp(alpha, -Mathf.Pi / 2, Mathf.Pi / 2)) / (-Mathf.Pi / 2 - alphaStallNeg);
+        }
+
+        float alphaEffective = alpha - zeroLiftAoA - Mathf.Lerp(0, coeffOfLift / (Mathf.Pi * AspectRatio), lerpParam);
+        float normalCoeff = _dragCoeffAtStall * Mathf.Sin(alphaEffective) * (1 / (.56f + .44f * Mathf.Sin(alphaEffective)) - .41f * _cNu);
+        float tangentCoeff = .5f * SkinFrictionCoeff * Mathf.Cos(alphaEffective);
+        coeffOfLift = normalCoeff * Mathf.Cos(alphaEffective) - tangentCoeff * Mathf.Sin(alphaEffective);
+        float coeffOfDrag = normalCoeff * Mathf.Sin(alphaEffective) + tangentCoeff * Mathf.Cos(alphaEffective);
+        float coeffOfTorque = -normalCoeff * (.25f - .175f * (1f - 2f * alphaEffective / Mathf.Pi));
+
+        return new Vector3(coeffOfLift, coeffOfDrag, coeffOfTorque);
     }
 }
--- singletons/Global.cs
+++ singletons/Global.cs
@@ -3,15 +3,17 @@ using Godot;
 public partial class Global : Node
 {
     [Export]
-    public float AirDensity = .001f;
+    public float AirDensity = 1.225f;
     [Export]
-    public float Viscosity = 1.4f;
+    public float Viscosity = 14f;
     [Export]
     public float LiftCurveSlope = 1.5f;
     [Export]
-    public float AlphaStallPosBase = 1.48f;
+    public float AlphaStallPosBase = .2617f;
     [Export]
-    public float AlphaStallNegBase = 1.48f;
+    public float AlphaStallNegBase = .2617f;
+    [Export]
+    public float DeltaCLMax = .2f;
 
     public string IpAddr = "127.0.0.1";
     public int Port = 8500;
